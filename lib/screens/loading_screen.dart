f1b499800a002b52a9dfd6c2b19321208ea1322e
Onwuzor Victor <victor.onwuzor@andela.com>
2020-07-08 20:10:30 +0100

    initial

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+const apiKey = '778d14d35550cecd2866a95a1240bfa0';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +13,44 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    getData();
+  }
+
+  void getData() async {
+    String url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02';
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      double temp = decodedData['main']['temp'];
+      int cond = decodedData['weather'][0]['id'];
+      String city = decodedData['name'];
+
+      print(temp);
+      print(cond);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
